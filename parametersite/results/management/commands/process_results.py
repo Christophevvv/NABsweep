be3689d3c3b8e258bd0d909c46895d2e16e84583
christophevvv <christophe.verdonck@gmail.com>
2019-02-11 22:17:40 +0100

    Updated models + work on addresult

---
+++ parametersite/results/management/commands/process_results.py
@@ -0,0 +1,112 @@
+#Processes results for given parameter
+import os
+import math
+import pandas
+import json
+
+class Processor:
+  def __init__(self,resultsDir ="results",detectorName="numenta",
+               configDir = "config", profilesFile="profiles.json"):
+    self.resultsDir = resultsDir
+    self.detectorName = detectorName
+    self.configDir = configDir
+    self.profilesFile = profilesFile
+    self.probationPercent = 0.15
+    #simplified absolute path of NAB
+    self.NABDir = os.path.realpath(os.path.join(os.path.dirname(__file__),'..','..','..','..','NAB'))
+    
+  def _getProbationaryLength(self, numRows):
+    return int(min(
+      math.floor(self.probationPercent * numRows),
+      self.probationPercent * 5000
+    ))
+    
+  def absoluteFilePaths(self,directory):
+    """Given directory, gets the absolute path of all files within.
+    
+    @param  directory   (string)    Directory name.
+
+    @return             (iterable)  All absolute filepaths within directory.
+    """
+    for dirpath,_,filenames in os.walk(directory):
+      filenames = [f for f in filenames if not f[0] == "."]
+      for f in filenames:
+        yield os.path.abspath(os.path.join(dirpath, f))
+
+  def getRelativePath(self,srcRoot,srcPath):
+    return srcPath[srcPath.index(srcRoot)+len(srcRoot):]\
+      .strip(os.path.sep).replace(os.path.sep, "/")
+  
+  def processLocalResults(self):
+    resultsDetectorDir = os.path.join(self.NABDir,self.resultsDir, self.detectorName)
+
+    filePaths = self.absoluteFilePaths(resultsDetectorDir)
+    dataSets = [(path,pandas.io.parsers.read_csv(path,header=0, parse_dates=[0])) for path in filePaths if ".csv" in path]
+    
+    for dataset in dataSets:
+      relativePath = self.getRelativePath(resultsDetectorDir,dataset[0])
+      data = dataset[1]
+
+      raw_score_no_anomaly = 0 
+      raw_score_during_anomaly = 0
+      path = relativePath.split("/")
+      if(len(path) == 2):
+        group = path[0]
+        file = path[1][len(self.detectorName+"_"):]
+      else:
+        continue
+      probationaryLength = self._getProbationaryLength(len(data))
+      #throw away all points before probation period
+      data = data[probationaryLength:]
+      raw_score_no_anomaly = data[data.label == 0][['raw_score']].sum(axis=0)
+      raw_score_during_anomaly = data[data.label == 1][['raw_score']].sum(axis=0)
+      # for i,row in data.iterrows():
+      #   if i >= probationaryLength:
+      #     if row["label"] == 0:
+      #       raw_score_no_anomaly += row["raw_score"]
+      #     else:
+      #       raw_score_during_anomaly += row["raw_score"]
+      #print(raw_score_no_anomaly)
+      #print(raw_score_during_anomaly)
+
+      yield group,file,len(data),raw_score_no_anomaly,raw_score_during_anomaly
+
+  def getProfiles(self):
+    profilePath = os.path.join(self.NABDir,self.configDir,self.profilesFile)
+    try:
+      profiles = json.load(open(profilePath,'r'))
+    except FileNotFoundError:
+      print("File not found: " + str(profilePath))
+      exit(1)
+    #print(profiles)
+    for profile in profiles:
+      yield profile,profiles[profile]['CostMatrix']
+
+
+  def getLocalProfileResults(self):
+    first = True
+    df = None
+    for profile in self.getProfiles():
+      filename = self.detectorName + "_" + profile[0] + "_scores.csv"
+      profileResultPath = os.path.join(self.NABDir,
+                                       self.resultsDir,
+                                       self.detectorName,
+                                       filename)
+      column_names = ['Detector','Profile','File','Threshold',
+                      profile[0]+'_Score',profile[0]+'_TP',
+                      profile[0]+'_TN',profile[0]+'_FP',
+                      profile[0]+'_FN','Total_Count']
+      profile_df = pandas.read_csv(profileResultPath,names=column_names)[1:-1]
+      if first:
+        keep_column = ['File',profile[0]+'_Score',profile[0]+'_TP',
+                      profile[0]+'_TN',profile[0]+'_FP',
+                      profile[0]+'_FN']
+        first=False
+        df = profile_df[keep_column]
+      else:
+        keep_column = [profile[0]+'_Score',profile[0]+'_TP',
+                      profile[0]+'_TN',profile[0]+'_FP',
+                      profile[0]+'_FN']
+        df = df.join(profile_df[keep_column])
+    return df
+      
--- parametersite/results/models.py
+++ parametersite/results/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
 class Dataset(models.Model):
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
     name = models.CharField(max_length = 50)
+    length = models.IntegerField()
 
     class Meta:
         unique_together = (('category','name'),)
@@ -45,6 +46,13 @@ class RunValue(models.Model):
     class Meta:
         unique_together = (('run','sweep_parameter','value'),)
         #Add constraint to link parameter with run.sweep -> sweepparameter?
+
+class Profile(models.Model):
+    name = models.CharField(max_length = 256,unique=True)
+    tp_weight = models.FloatField()
+    fp_weight = models.FloatField()
+    tn_weight = models.FloatField()
+    fn_weight = models.FloatField()
     
 class LocalResult(models.Model):
     ''' The result for a given sweep run for each dataset individually '''
@@ -52,10 +60,32 @@ class LocalResult(models.Model):
     dataset = models.ForeignKey('Dataset',on_delete=models.CASCADE)
     pred_error_no_anomaly = models.FloatField()
     pred_error_during_anomaly = models.FloatField()
+
+    class Meta:
+        unique_together = (('run','dataset'),)    
+
+class LocalResultScore(models.Model):
+    local_result = models.ForeignKey('LocalResult',on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile',on_delete=models.CASCADE)
+    score = models.FloatField()
+    true_positives = models.IntegerField()
+    true_negatives = models.IntegerField()
+    false_positives = models.IntegerField()
+    false_negatives = models.IntegerField()
+
+    class Meta:
+        unique_together = (('local_result','profile'),)        
     
 class GlobalResult(models.Model):
     ''' The result for a given sweep for all datasets together '''
     run = models.ForeignKey('Run',on_delete=models.CASCADE)
-    pred_error_no_anomaly = models.FloatField()
-    pred_error_during_anomaly = models.FloatField()
 
+class GlobalResultScore(models.Model):
+    #You can aggregate localresult scores to get total score/tp/tn/fp/fn...
+    global_result = models.ForeignKey('GlobalResult',on_delete=models.CASCADE)
+    profile = models.ForeignKey('Profile',on_delete=models.CASCADE)
+    threshold = models.FloatField()
+    normalized_score = models.FloatField() #0-100
+
+    class Meta:
+        unique_together = (('global_result','profile'),)
