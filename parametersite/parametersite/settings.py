968468d74a2a2f4f1bd8ae2c0a25ee6255a54881
christophevvv <christophe.verdonck@gmail.com>
2019-02-12 17:06:32 +0100

    Moved from sqlite3 to mysql

--- parametersite/parametersite/settings.py
+++ parametersite/parametersite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'parametersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NABsweep',
+        'USER': 'nabsweep',
+        'PASSWORD': 'nabsweep',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
